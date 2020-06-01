06331784b40182186395e466b94330af56f918be
ChillerDragon <ChillerDragon@gmail.com>
2020-06-01 20:33:14 +0200

    Start working on demo header parsing

---
+++ demo.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+#
+# demo.sh <demofile>
+#
+
+# struct CDemoHeader
+# {
+# 	unsigned char m_aMarker[7];
+# 	unsigned char m_Version;
+# 	char m_aNetversion[64];
+# 	char m_aMapName[64];
+# 	unsigned char m_aMapSize[4];
+# 	unsigned char m_aMapCrc[4];
+# 	char m_aType[8];
+# 	unsigned char m_aLength[4];
+# 	char m_aTimestamp[20];
+# 	unsigned char m_aNumTimelineMarkers[4];
+# 	unsigned char m_aTimelineMarkers[MAX_TIMELINE_MARKERS][4];
+# };
+
+H_MAGIC='TWDEMO'
+H_MAGIC_LEN=7
+H_DEMO_VERSION=$((H_MAGIC_LEN + 1))
+H_NETVERISON_LEN=64
+H_NETVERISON_END=$((
+    H_DEMO_VERSION +
+    H_NETVERISON_LEN
+))
+H_MAPNAME_LEN=64
+H_MAPNAME_END=$((
+    H_MAGIC_LEN +
+    1 +
+    64 +
+    H_MAPNAME_LEN
+))
+H_MAPSIZE_LEN=4
+H_MAPSIZE_END=$((
+    H_MAPNAME_END +
+    H_MAPSIZE_LEN
+))
+H_MAPCRC_LEN=4
+H_MAPCRC_END=$((
+    H_MAPSIZE_END +
+    H_MAPCRC_LEN
+))
+H_TYPE_LEN=8
+H_TYPE_END=$((
+    H_MAPCRC_END +
+    H_TYPE_LEN
+))
+H_LENGTH_LEN=4
+H_LENGTH_END=$((H_TYPE_END + H_LENGTH_LEN))
+H_TIME_LEN=25 # TODO: why is this not 20?
+H_TIME_END=$((H_TYPE_END + H_TIME_LEN))
+H_NUM_MARKERS_LEN=4
+H_NUM_MARKERS_END=$((
+    H_TIME_END +
+    H_NUM_MARKERS_LEN
+))
+
+if [ "$#" -lt "1" ]
+then
+    echo "Usage: $(basename "$0") <demofile>"
+    echo "outputs demo 0.7 header"
+    exit 1
+fi
+demofile="$1"
+if [ ! -f "$demofile" ]
+then
+    echo "Error: file '$demofile' does not exist"
+    exit 1
+fi
+header_magic=$(head -c 6 "$demofile")
+header_netversion=$(head -c $H_NETVERISON_END "$demofile" | tail -c $H_NETVERISON_LEN)
+header_mapname=$(head -c $H_MAPNAME_END "$demofile" | tail -c $H_MAPNAME_LEN)
+header_type=$(head -c $H_TYPE_END "$demofile" | tail -c $H_TYPE_LEN)
+header_time=$(head -c $H_TIME_END "$demofile" | tail -c $H_TIME_LEN)
+header_num_markers=$(head -c $H_NUM_MARKERS_END "$demofile" | tail -c $H_NUM_MARKERS_LEN | xxd)
+if [ "$header_magic" != "$H_MAGIC" ]
+then
+    echo "Error: invalid demo file '$header_magic' != '$H_MAGIC'"
+    exit 1
+fi
+
+echo "netversion: $header_netversion"
+echo "map: $header_mapname"
+echo "type: $header_type"
+echo "timestamp: $header_time"
+echo "markers: $header_num_markers"
+
